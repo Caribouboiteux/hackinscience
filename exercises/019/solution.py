19e553d2c9c62cd631ed87ec7857ac84ff7ba716
Caribouboiteux <ccaribou@sfr.fr>
2015-10-15 16:40:53 +0200

    VICTOIRE

---
+++ exercises/019/solution.py
@@ -0,0 +1,10 @@
+import math
+import sys 
+if len(sys.argv)!=3: 
+    print ("usage: python3 solution.py OP1 OP2")
+else:
+    a=int(sys.argv[1])
+    b=int(sys.argv[2])
+    c=str(a+b)
+    print (c) 
+
