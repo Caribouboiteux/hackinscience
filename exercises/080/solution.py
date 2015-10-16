29905e3eab3b73759371ff8370027b1492be6063
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 22:09:10 +0200

    le mauvais trébuche toujours deux fois sur la même pierre

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -1,6 +1,6 @@
 from itertools import combinations_with_replacement
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-for (a, b) in combinations_with_replacement(alphabets, 2):
+for (a, b) in combinations_with_replacement(alphabet, 2):
     if a != b:
         c = a + b
         print(c)
