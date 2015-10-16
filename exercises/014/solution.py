0d6b39896b94c17ad5fa20ba424a19dfbd43a07f
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 16:21:17 +0200

    Bloug

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,5 +1,5 @@
-import sys 
+import sys
 if len(sys.argv)<2:
-    print ("usage: python3 PARAM.solution py") 
-else :
-    print sys.argv[1]
+    print("usage: python3 PARAM.solution py")
+else:
+    print(sys.argv[1])
