0ee9c923c6537f41a8339dfada7293eaa5a1dcc2
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 17:06:11 +0200

    Yahou

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,7 +1,7 @@
 a = 0
-for i in range(999):
-    if int(i) % 3:
+for i in range(1000):
+    if int(i) % 3 == 0:
         a = a + i
-    if int(i) % 5:
+    if int(i) % 5 == 0:
         a = a + i
-print(a)
+    print(a)
