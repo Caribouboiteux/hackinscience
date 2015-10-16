62b9aaa278f59738a3e29880ebaa8dd83c894e33
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 22:07:06 +0200

    Nah mais y'avais des rêgles que je connaissais pas aussi !

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -1,7 +1,5 @@
 from itertools import combinations_with_replacement
-
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 for (a, b) in combinations_with_replacement(alphabets, 2):
     if a != b:
         c = a + b
