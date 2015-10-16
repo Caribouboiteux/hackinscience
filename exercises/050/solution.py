1738faed8ac65614369f8e4fa759eab0e6ebb014
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 21:57:29 +0200

    Voilà !

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,7 +1,6 @@
 a = 0
 for i in range(1000):
-    if int(i) % 3 == 0:
+    if (int(i) % 3 == 0) or (int(i) % 5 == 0):
         a = a + i
-    if int(i) % 5 == 0:
-        a = a + i
-    print(int(a))
+b = int(a)
+print(a)
