7be67d9e7c65b64b0e87d692ef170591e7c19e92
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 16:38:03 +0200

    Bibou

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,5 +1,5 @@
 import datetime
 import time
-a=(datetime.date.today()) 
-b=time.strftime("%H:%M:%S")
-print "Today is",a,"and it is",b
+a = (datetime.date.today())
+b = (time.strftime("%H:%M:%S"))
+print("Today is",a,"and it is",b)
