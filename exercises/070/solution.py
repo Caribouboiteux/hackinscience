cca13676ce0aeb3fcab2a6da28893f48185e2edb
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 22:00:53 +0200

    Group

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -1,8 +1,8 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-for lettre in alphabet: 
-    a=lettre
+for lettre in alphabet:
+    a = lettre
     for lettre in alphabet:
-        if a!=lettre :
-            b=lettre
-            c=a+b 
-            print c 
+        if a != lettre:
+            b = lettre
+            c = a + b
+            print(c)
