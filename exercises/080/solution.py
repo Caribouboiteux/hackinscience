6fe049d659b4b086d7c005985d44e1472d48e560
Caribouboiteux <ccaribou@sfr.fr>
2015-10-16 22:03:25 +0200

    Je mise du premier coup

--- exercises/080/solution.py
+++ exercises/080/solution.py
@@ -2,10 +2,7 @@ from itertools import combinations_with_replacement
 
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-for (a,b) in combinations_with_replacement(alphabets, 2):
-    if a != b :
-        c = a + b 
-        print c
-
-
-
+for (a, b) in combinations_with_replacement(alphabets, 2):
+    if a != b:
+        c = a + b
+        print(c)
