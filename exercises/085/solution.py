03539f44f4e39fd0dad9f4ba4dbadff25eefe343
Caribouboiteux <ccaribou@sfr.fr>
2015-10-20 14:52:13 +0200

    Ceci est le travail de 5jours..

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,12 +1,14 @@
 def sort_a_list(a):
     a.sort(reverse=True)
 
+
 def sort_by_mark(a):
     a.sort(reverse=True, key=lambda colonne: colonne[0])
     b = str(a)
     c = b.split("],")
     d = "]\n".join(c)
     print(d)
+
 
 def sort_by_name(a):
     a.sort(key=lambda colonne: colonne[1])
