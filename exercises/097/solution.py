27c57cdb1296c64d3491843a49aa599951e0d337
Caribouboiteux <ccaribou@sfr.fr>
2015-10-23 14:17:06 +0200

    easyyyyyyyy

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -1,3 +1,8 @@
 def love_meet(perso1, perso2):
     a = set(perso1) & set(perso2)
     return(a)
+
+def affair_meet(bob, alice, silvester):
+    a = set(alice) & set(silvester)
+    b = set(a).difference(bob)
+    return(b)
