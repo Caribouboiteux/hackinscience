8062689a0ab131b77b6edade5ede36343a955c2a
Caribouboiteux <ccaribou@sfr.fr>
2015-11-05 23:30:58 +0100

    Du premier coup ? Pick and choose

--- exercises/solution.py
+++ exercises/solution.py
@@ -1,1 +1,16 @@
-print("abcdefghijklnmopqrstuvwxyz")
+def select_student(liste, note_minimum):
+    sortie = {}
+    accepte = []
+    refuse = []
+    for etudiant in liste:
+        note = etudiant[1]
+        if note < note_minimum:
+            refuse.append(etudiant)
+        else:
+            accepte.append(etudiant)
+    accepte = sorted(accepte, reverse=True, key=lambda colonne: colonne[1])
+    refuse = sorted(refuse, key=lambda colonne: colonne[1])
+    sortie['Refused'] = refuse
+    sortie['Accepted'] = accepte
+    return sortie
+            
