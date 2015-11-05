f6e143ece361396486ad1dc2eb634d1e42093a8c
Caribouboiteux <ccaribou@sfr.fr>
2015-11-06 00:45:21 +0100

    Roi du monde je calcule c'que j'veux

---
+++ exercises/110/solution.py
@@ -0,0 +1,28 @@
+import math
+import sys
+if len(sys.argv) != 4:
+    print("usage: ./solution.py a_number (an_operator +-*/%^) a_number")
+else:
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if str(sys.argv[2]) == "*":
+            c = str(a * b)
+            print(c)
+        if str(sys.argv[2]) == "+":
+            c = str(a + b)
+            print(c)
+        if str(sys.argv[2]) == "-":
+            c = str(a - b)
+            print(c)
+        if str(sys.argv[2]) == "/":
+            c = str(a / b)
+            print(c)
+        if str(sys.argv[2]) == "%":
+            c = str(a % b)
+            print(c)
+        if str(sys.argv[2]) == "^":
+            c = str(a ** b)
+            print(c)
+    except:
+        print("input error")
