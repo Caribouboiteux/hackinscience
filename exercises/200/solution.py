fd190f2f64418bdeca15c97bb1706f0040e7ee22
Caribouboiteux <ccaribou@sfr.fr>
2015-11-06 13:20:24 +0100

    Test internet pour nombre premier

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -1,7 +1,9 @@
 def is_prime(n):
-    if n % 2 == 0: return False
+    if n % 2 == 0:
+        return False
     p = 3
     while p < n**0.5+1:
-        if n % p == 0: return False
+        if n % p == 0:
+            return False
         p += 2
     return True
