fdbb5b0d62abb472110efe0e2f4fbef1d4feedda
Caribouboiteux <ccaribou@sfr.fr>
2015-11-06 16:42:41 +0100

    nombre premier

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -1,11 +1,11 @@
 def is_prime(n):
     if n % 2 == 0:
-        return False
+        return(False)
     else:
         a = n - 1
         for i in range(1, a):
             s = n % i
             if s == 0:
-                return False
+                return(False)
             else:
-                return True
+                return(True)
