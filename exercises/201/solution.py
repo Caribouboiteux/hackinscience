dc9d94dfe5a17b65c94dfdfdd2229b7d22668d50
Caribouboiteux <ccaribou@sfr.fr>
2015-11-09 23:34:08 +0100

    Du premier coup ?3

--- exercises/201/solution.py
+++ exercises/201/solution.py
@@ -1,7 +1,6 @@
 def is_alpha(chaine):
     lettres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for i in chaine:
-        if i in lettres:
-            return True
-        else:
+        if i not in lettres:
             return False
+    return True
