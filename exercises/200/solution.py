b5239f72d1a8a8e0c44b8578e1ad65efa97af6ff
Caribouboiteux <ccaribou@sfr.fr>
2015-11-09 23:11:56 +0100

    Copier coller internet nul

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -1,15 +1,6 @@
 def is_prime(n):
-    if n == 2:
-        return True
-    if n == 1:
-        return False
-    if n % 2 == 0:
-        return False
-    else:
-        for i in range(2, 50):
-            print(i)
-            s = n % i
-            print(s)
-            if s == 0 and n != i:
-                return False
-        return True
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+
+    return True
