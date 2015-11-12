2eb9db46ec7a7f576eeb914b76bdf658ecb5c21b
Caribouboiteux <ccaribou@sfr.fr>
2015-11-12 13:51:09 +0100

    OOOOH YES !

--- exercises/250/solution.py
+++ exercises/250/solution.py
@@ -5,8 +5,5 @@ def draw_n_squares(n):
     d = "   |"
     e = a + (int(n) - 1) * b
     f = c + (int(n) - 1) * d
-    print(str(e))
-    u = int(n)
-    for i in range(0, u):
-        print(str(f))
-        print(str(e))
+    sortie = (e + "\n" + f + "\n") * n + e
+    print(sortie)
