416ec1edd9a143554a3007dd44ca6fe66ad46da7
Caribouboiteux <ccaribou@sfr.fr>
2015-12-02 00:14:23 +0100

    Coucouuuu

--- exercises/332/solution.py
+++ exercises/332/solution.py
@@ -6,6 +6,6 @@ data = json.load(json_data)
 map_osm = folium.Map(location=[48.8535, 2.3454], zoom_start=12)
 for i in data:
     a = i["address"] + " " + i["name"]
-    map_osm.simple_marker([i["latitude"], i["longitude"]], popup = a)
+    map_osm.simple_marker([i["latitude"], i["longitude"]], popup=a)
 map_osm.create_map(path='velib.html')
 json_data.close()
