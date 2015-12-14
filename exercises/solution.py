608129bdaa17e6840a0863a8102f10f6aa4eb2b3
Caribouboiteux <ccaribou@sfr.fr>
2015-12-14 14:54:38 +0100

    Juste une sauvegarde en ligne

--- exercises/solution.py
+++ exercises/solution.py
@@ -1,16 +1,57 @@
-def select_student(liste, note_minimum):
-    sortie = {}
-    accepte = []
-    refuse = []
-    for etudiant in liste:
-        note = etudiant[1]
-        if note < note_minimum:
-            refuse.append(etudiant)
-        else:
-            accepte.append(etudiant)
-    accepte = sorted(accepte, reverse=True, key=lambda colonne: colonne[1])
-    refuse = sorted(refuse, key=lambda colonne: colonne[1])
-    sortie['Refused'] = refuse
-    sortie['Accepted'] = accepte
-    return sortie
-            
+def method():
+    if str(method) == "forward":
+        method = "forward"
+    if str(method) == "backward":
+        method = "backward"
+
+
+def caesar_cypher(s, key, method):
+    a = method
+    sortie = []
+    return type(method)
+    if method == "forward":
+        for i in s:
+            ascii = ord(i)
+            if ascii > 64:
+                if ascii < 91:
+                    new_ascii = ascii
+                    for k in range(0, key):
+                        new_ascii = new_ascii + 1
+                        if new_ascii > 90:
+                            new_ascii = 65
+                    sortie.append(chr(int(new_ascii)))
+                if ascii > 96:
+                    if ascii < 123:
+                        new_ascii = ascii
+                        for k in range(0, key):
+                            new_ascii = new_ascii + 1
+                            ascii = new_ascii
+                            if new_ascii > 122:
+                                new_ascii = 97
+                        sortie.append(chr(int(new_ascii)))
+            else:
+                sortie.append(i)
+        return "".join(sortie)
+    if method == "backward":
+        for i in s:
+            ascii = ord(i)
+            if ascii > 64:
+                if ascii < 91:
+                    new_ascii = ascii
+                    for k in range(0, key):
+                        new_ascii = new_ascii - 1
+                        if new_ascii < 65:
+                            new_ascii = 90
+                    sortie.append(chr(int(new_ascii)))
+                if ascii > 96:
+                    if ascii < 123:
+                        new_ascii = ascii
+                        for k in range(0, key):
+                            new_ascii = new_ascii - 1
+                            ascii = new_ascii
+                            if new_ascii < 97:
+                                new_ascii = 122
+                        sortie.append(chr(int(new_ascii)))
+            else:
+                sortie.append(i)
+        return "".join(sortie)
