7a82224e876ef8edb1a021422cac3d13bb47f6f1
Caribouboiteux <ccaribou@sfr.fr>
2015-12-15 12:26:50 +0100

    cette fois c'est bon, patate ?

--- exercises/440/solution.py
+++ exercises/440/solution.py
@@ -7,6 +7,6 @@ def filtered(items, fonction_conscise):
 
 
 if __name__ == '__main__':
-    print(", ".join(filtered(range(0, 101), lambda x: x % 3 == 0)))
-    print(", ".join(filtered(range(0, 101), lambda x: x % 5 == 0)))
-    print(", ".join(filtered(range(0, 101), lambda x: x % 15 == 0)))
+    print(filtered(range(0, 101), lambda x: x % 3 == 0))
+    print(filtered(range(0, 101), lambda x: x % 5 == 0))
+    print(filtered(range(0, 101), lambda x: x % 15 == 0))
