a7fe560e04e021d405eb602e6c069749a14d8aaa
Caribouboiteux <ccaribou@sfr.fr>
2015-12-15 10:41:33 +0100

    COucou

---
+++ exercises/450/solution.py
@@ -0,0 +1,55 @@
+def forward(s, key):
+    sortie = []
+    for i in s:
+        ascii = ord(i)
+        if ascii > 64:
+            if ascii < 91:
+                new_ascii = ascii
+                print(new_ascii)
+                for k in range(0, key):
+                    new_ascii = new_ascii + 1
+                    if new_ascii > 90:
+                        new_ascii = 65
+                sortie.append(chr(int(new_ascii)))
+            if ascii > 96:
+                if ascii < 123:
+                    new_ascii = ascii
+                    for k in range(0, key):
+                        new_ascii = new_ascii + 1
+                        ascii = new_ascii
+                        if new_ascii > 122:
+                            new_ascii = 97
+                    sortie.append(chr(int(new_ascii)))
+        else:
+            sortie.append(i)
+    return "".join(sortie)
+
+
+def backward(s, key):
+    sortie = []
+    for i in s:
+        ascii = ord(i)
+        if ascii > 64:
+            if ascii < 91:
+                new_ascii = ascii
+                for k in range(0, key):
+                    new_ascii = new_ascii - 1
+                    if new_ascii < 65:
+                        new_ascii = 90
+                sortie.append(chr(int(new_ascii)))
+            if ascii > 96:
+                if ascii < 123:
+                    new_ascii = ascii
+                    for k in range(0, key):
+                        new_ascii = new_ascii - 1
+                        ascii = new_ascii
+                        if new_ascii < 97:
+                            new_ascii = 122
+                    sortie.append(chr(int(new_ascii)))
+        else:
+            sortie.append(i)
+    return "".join(sortie)
+
+
+def caesar_cypher(s, key, method):
+    method(s, key)
