fc648a2051e661986b87d6281ccf891587f23480
Caribouboiteux <ccaribou@sfr.fr>
2016-01-20 16:30:15 +0100

    Euler fou

--- exercises/500/solution.py
+++ exercises/500/solution.py
@@ -22,12 +22,12 @@ grid = [int(i) for i in grid.split()]
 sortie = []
 c = 0
 for i in range(400):
-    if i%20 < 17:
-        sortie.append(grid[i]*grid[i+1]*grid[i+2]*grid[i+3])
+    if i % 20 < 17:
+        sortie.append(grid[i]*grid[i + 1]*grid[i + 2]*grid[i + 3])
     if i < 340:
-        sortie.append(grid[i]*grid[i+20]*grid[i+40]*grid[i+60])
-    if i%20 < 17 and i < 340:
-        sortie.append(grid[i]*grid[i+21]*grid[i+42]*grid[i+63])
-    if i%20 > 3 and i < 340:
-        sortie.append(grid[i]*grid[i+19]*grid[i+38]*grid[i+57])
+        sortie.append(grid[i]*grid[i + 20]*grid[i + 40]*grid[i + 60])
+    if i % 20 < 17 and i < 340:
+        sortie.append(grid[i]*grid[i + 21]*grid[i + 42]*grid[i + 63])
+    if i % 20 > 3 and i < 340:
+        sortie.append(grid[i]*grid[i + 19]*grid[i + 38]*grid[i + 57])
 print(max(sortie))
